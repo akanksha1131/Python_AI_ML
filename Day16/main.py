459a9829bf6fcdaaa69190c40476f65117fa9c3f
akanksha <akanksha.wrk@gmail.com>
2024-01-07 10:42:08 +0530

    turtle

---
+++ Day16/main.py
@@ -0,0 +1,8 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.add_column("City", ["Pune", "Mumbai", "Hyderabad"])
+table.add_column("State", ["Maharashtra", "Maharashtra", "Telangana"])
+table.align='l'
+print(table)
+
